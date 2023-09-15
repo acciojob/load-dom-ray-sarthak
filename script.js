a58aa66808ba5f627b336ff5db9f43f51d288652
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-16 01:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.onload=(event)=>{
+	const h1=document.createElement("h1");
+	h1.textContent="DOM load success";
+	const body=document.getElementsByTagName("body")[0];
+	body.appendChild(h1);
+}
